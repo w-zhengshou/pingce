a1f490951d85ffb133147b15ea6145e7a7084930
w-zhengshou <zhengshou@ek12.com>
2020-03-02 13:53:39 +0800

    111

---
+++ MyApp/MyApp/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  MyApp
+//
+//  Created by 万争寿 on 2020/3/2.
+//  Copyright © 2020 万争寿. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
